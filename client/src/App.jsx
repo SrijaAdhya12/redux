27447938f5cdd26310088c312f547a66116fc343
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-27 09:32:16 +0530

    feat: formated code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import AddTodo from "./components/AddTodo"
-import Todos from "./components/Todos"
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 const App = () => {
 	return (
 		<div className="mx-48 my-32 flex flex-col justify-center w-1/2 ">
 			<h1 className="text-3xl font-bold pb-5">Todo App</h1>
 			<AddTodo />
-			<Todos/>
+			<Todos />
 		</div>
 	)
 }
--- client/src/features/todo/TodoSlice.js
+++ client/src/features/todo/TodoSlice.js
@@ -1,24 +1,24 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = {
-    todos: [{id:1, text: "Work"}]
+	todos: [{ id: 1, text: 'Work' }]
 }
 
 export const todoSlice = createSlice({
-    name: "todo",
-    initialState,
-    reducers: {
-        addTodo: (state, action) => {
-            const todo = {
+	name: 'todo',
+	initialState,
+	reducers: {
+		addTodo: (state, action) => {
+			const todo = {
 				id: nanoid(),
-				text: action.payload,
-            }
-            state.todos.push(todo)
-        },
-        deleteTodo: (state, action) => {
-            state.todos = state.todos.filter(todo => todo.id !== action.payload)
-        }
-    }
+				text: action.payload
+			}
+			state.todos.push(todo)
+		},
+		deleteTodo: (state, action) => {
+			state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+		}
+	}
 })
 
 export const { addTodo, deleteTodo } = todoSlice.actions
